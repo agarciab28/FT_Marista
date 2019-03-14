02a3c1c3c7dc67f96366da1de9967b3690d70457
agarciab28 <agarciab28@gmail.com>
2019-03-14 16:21:22 -0600

    Buscar Paciente 2

--- AppConsultas/resources/views/medico/buscar.blade.php
+++ AppConsultas/resources/views/medico/buscar.blade.php
@@ -8,5 +8,72 @@
 @section('title', 'Buscar Paciente')
 
 @section('content')
+  <form class="section" action="" method="">
+    <div class="row">
+      <div class="input-field col s10 m4 push-m4">
+        <input id="buscador" type="text">
+        <label for="buscador"><i class="fas fa-search" style="margin-right:0.5em;"></i>Buscar</label>
+      </div>
+      {{-- <button class="btn waves-effect waves-light" type="submit" name="action"><i class="fas fa-search"></button>
+    </div> --}}
+  </form>
+
+  <div class="row">
+    <div class="col s12">
+      <table class="striped responsive-table">
+     <thead>
+       <tr>
+           <th>Nombre</th>
+           <th>Apellido</th>
+           <th>CURP</th>
+           <th>Estado</th>
+           <th>Acciones</th>
+       </tr>
+     </thead>
+     <tbody>
+       <tr>
+         <td>Alejandro</td>
+         <td>García</td>
+         <td>GABAasedfr12345678</td>
+         <td>Rehabilitacion</td>
+         <td> <a href="" class="waves-effect waves-light btn">Ver Identificacion</a></td>
+       </tr>
+       <tr>
+         <td>Alejandro</td>
+         <td>García</td>
+         <td>GABAasedfr12345678</td>
+         <td>Rehabilitacion</td>
+         <td> <a href="" class="waves-effect waves-light btn">Ver Identificacion</a></td>
+       </tr>
+       <tr>
+         <td>Alejandro</td>
+         <td>García</td>
+         <td>GABAasedfr12345678</td>
+         <td>Rehabilitacion</td>
+         <td> <a href="" class="waves-effect waves-light btn">Ver Identificacion</a></td>
+       </tr>
+       <tr>
+         <td>Alejandro</td>
+         <td>García</td>
+         <td>GABAasedfr12345678</td>
+         <td>Rehabilitacion</td>
+         <td> <a href="" class="waves-effect waves-light btn">Ver Identificacion</a></td>
+       </tr>
+       <tr>
+         <td>Alejandro</td>
+         <td>García</td>
+         <td>GABAasedfr12345678</td>
+         <td>Rehabilitacion</td>
+         <td> <a href="" class="waves-effect waves-light btn">Ver Identificacion</a></td>
+       </tr>
+       <tr>
+         <td>Alejandro</td>
+         <td>García</td>
+         <td>GABAasedfr12345678</td>
+         <td>Rehabilitacion</td>
+         <td> <a href="" class="waves-effect waves-light btn">Ver Identificacion</a></td>
+       </tr>
+     </tbody>
+   </table>
 
 @endsection
