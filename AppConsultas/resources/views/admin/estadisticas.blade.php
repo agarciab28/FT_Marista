0fb22b6f9c557db9d2b008481a66c71c9e38adba
agarciab28 <agarciab28@gmail.com>
2019-03-14 12:17:25 -0600

    Estadisticas admin

--- AppConsultas/resources/views/admin/estadisticas.blade.php
+++ AppConsultas/resources/views/admin/estadisticas.blade.php
@@ -8,5 +8,34 @@
 @section('title', 'Estadisticas')
 
 @section('content')
+  <div class="row section container">
+    <div class="contenedor col s6 push-s3">
+      <h3>Edades</h3>
+    </div>
+  </div>
+  <canvas id="chart1" width="400" height="200"></canvas>
+  <div class="row section container">
+    <div class="contenedor col s6 push-s3">
+      <h3>Genero</h3>
+    </div>
+  </div>
+  <canvas id="chart2" width="400" height="200"></canvas>
+  <div class="row section container">
+    <div class="contenedor col s6 push-s3">
+      <h3>Sintomas Generales</h3>
+    </div>
+  </div>
+  <canvas id="chart3" width="400" height="200"></canvas>
+  <div class="row section container">
+    <div class="contenedor col s6 push-s3">
+      <h3>Aparatos y Sistemas</h3>
+    </div>
+  </div>
+  <canvas id="chart4" width="400" height="200"></canvas>
+@endsection
 
+@section('scripts')
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.bundle.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js"></script>
+  <script type="text/javascript" src="{{{ asset('js/estadisticas/test.js') }}}"></script>
 @endsection
