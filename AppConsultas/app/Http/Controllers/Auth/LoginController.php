fc12f00e3ccc3d0776ef36c17966c81479f5d507
Alexis Herrera <alex.herrera361@gmail.com>
2019-04-07 20:24:33 -0500

    Login 80% backend

--- AppConsultas/app/Http/Controllers/Auth/LoginController.php
+++ AppConsultas/app/Http/Controllers/Auth/LoginController.php
@@ -1,39 +1,24 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Auth;
 use App\Http\Controllers\Controller;
-use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
+  public function login(){
+    $credentials = $this->validate(request(), [
+      'curp' => 'required|string',
+      'password' => 'required|string'
+    ]);
 
-    use AuthenticatesUsers;
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
+    if(Auth::attempt($credentials)){
+      return view('dashboard');
     }
+    return back()
+    ->withErrors(['btn_s' => 'CURP o contraseña inválidos'])
+    ->withInput(request(['curp']));
+  }
+
+
 }
